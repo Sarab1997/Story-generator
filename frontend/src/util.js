e0761c8f13aa3549339bb37eedea39db6ab445e7
Sarabjot Singh <onlinesarab@outlook.com>
2025-12-07 15:04:23 -0800

    Add production config

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,2 +1,4 @@
-// util.js
-export const API_BASE_URL = "http://localhost:8000/api"
+// // util.js
+// export const API_BASE_URL = "http://localhost:8000/api"
+
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api"
